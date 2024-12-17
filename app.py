6f90a1ff1922ca7c7019b174cfd22c0599625eed
NANJI FREDDY <nanjifreddy2@gmail.com>
2024-12-17 22:02:44 +0100

    fix: flask interface designing

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
 import pandas as pd
 from src.datascience.pipeline.prediction_pipeline import PredictionPipeline
 
-
-app = Flask(__name__)  # initializing a flask app
-
-
-@app.route('/', methods=['GET'])  # route to display the home page
-def homePage():
-    return render_template("index.html")
+app = Flask(__name__)
 
 
-@app.route('/train', methods=['GET'])  # route to train the pipeline
-def training():
-    os.system("python main.py")
-    return "Training Successful!"
-
-
-# route to show the predictions in a web UI
-@app.route('/predict', methods=['POST', 'GET'])
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def home():
     if request.method == 'POST':
         try:
-            #  reading the inputs given by the user
             fixed_acidity = float(request.form['fixed_acidity'])
             volatile_acidity = float(request.form['volatile_acidity'])
             citric_acid = float(request.form['citric_acid'])
@@ -37,23 +23,24 @@ def index():
             sulphates = float(request.form['sulphates'])
             alcohol = float(request.form['alcohol'])
 
-            data = [fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides,
-                    free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]
-            data = np.array(data).reshape(1, 11)
+            data = np.array([fixed_acidity, volatile_acidity, citric_acid, residual_sugar,
+                             chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density,
+                             pH, sulphates, alcohol]).reshape(1, -1)
 
             obj = PredictionPipeline()
             predict = obj.predict(data)
-
-            return render_template('results.html', prediction=str(predict))
-
+            return jsonify({"prediction": predict[0]})
         except Exception as e:
-            print('The Exception message is: ', e)
-            return 'something is wrong'
+            print(f"Exception: {e}")
+            return jsonify({"error": "Invalid input or prediction error."})
+    return render_template("index.html")
 
-    else:
-        return render_template('index.html')
+
+@app.route('/train', methods=['GET'])
+def train():
+    os.system("python main.py")
+    return jsonify({"message": "Model training successful!"})
 
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=8080)
